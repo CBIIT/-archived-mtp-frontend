b9467f93c89e19f58b58673e732276c380ee9867
Chen <jonkiky@gmail.com>
2022-10-25 10:22:35 -0400

    build the static pages

---
+++ build/config.js
@@ -0,0 +1,5 @@
+// Default configuration is defined within the application bundle
+window.injectedEnv = {
+  configUrlApi: 'https://ppdc-otp-dev.bento-tools.org/api/v4/graphql',
+  configUrlApiBeta: 'https://ppdc-otp-dev.bento-tools.org/api/v4/graphql',
+};
